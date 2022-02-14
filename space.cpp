b697f61f2b6a6e41924248a05fd88784094126db
Amar_Zaidi <amarzaidi12321@gmail.com>
2022-02-13 17:21:50 -0800

    Files Updated
    Screen is adjusted and load board and print board functions are implemented

--- space.cpp
+++ space.cpp
@@ -1,8 +1,32 @@
 #include<iostream>
 #include<fstream>
 using namespace std;
-char board[40][80];
+void load_screen();
+void print_Screen();
+char board[38][100];
 int main(){
 
 
+}
+void load_screen(){
+    string line = " ";
+    int row_idx = 0;
+    fstream file;
+    file.open("screen.txt",ios::in);
+    while(!file.eof()){
+        file>>line;
+        for(int col_idx = 0;col_idx<line.length();col_idx++){
+        board[row_idx][col_idx] = line[col_idx];
+        }
+        row_idx++;
+    }
+    file.close();
+}
+void print_Screen(){
+    for(int row_idx = 0;row_idx < 38){
+        for(int col_idx = 0;col_idx < 100;col_idx++){
+            cout << board[row_idx][col_idx];
+        }
+        cout << endl;
+    }
 }
