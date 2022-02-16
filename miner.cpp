72e7a5116cbd023cfdc3abdcd93889285202ad8b
Amar_Zaidi <amarzaidi12321@gmail.com>
2022-02-16 09:35:11 -0800

    Files Updated
    Successfully Implemented functions for moving in right and left using loop

--- miner.cpp
+++ miner.cpp
@@ -10,6 +10,12 @@ void gotoxy(int x, int y);
 void loadMaze();
 void printMaze();
 void minerMoveLeft();
+void minerMoveRight();
+bool isRightMovePossible(int temp_row_idx, int temp_col_idx);
+bool isLeftMovePossible(int temp_row_idx, int temp_col_idx);
+// Temperary Checks Varaible
+string MoveRightStatus = "Alert";
+string MoveLeftStatus = "Alert";
 int main()
 {
     bool gameRunning = true;
@@ -18,10 +24,14 @@ int main()
     printMaze();
     while (gameRunning)
     {
-        Sleep(100);
+        Sleep(50);
         if (GetAsyncKeyState(VK_LEFT))
         {
             minerMoveLeft();
+        }
+        else if (GetAsyncKeyState(VK_RIGHT))
+        {
+            minerMoveRight();
         }
     }
     return 0;
@@ -61,31 +71,59 @@ void gotoxy(int x, int y)
     coordinates.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinates);
 }
+bool isRightMovePossible(int temp_row_idx, int temp_col_idx)
+{
+    int temp_count = 0;
+    if (maze[temp_row_idx][temp_col_idx + 1] == ' ')
+    {
+        temp_count++;
+    }
+    if (maze[temp_row_idx + 1][temp_col_idx + 1] == ' ')
+    {
+        temp_count++;
+    }
+    if (maze[temp_row_idx + 2][temp_col_idx + 1] == ' ')
+    {
+        temp_count++;
+    }
+    if (temp_count == 3)
+    {
+        return true;
+    }
+    return false;
+}
+bool isLeftMovePossible(int temp_row_idx, int temp_col_idx)
+{
+    int temp_count = 0;
+    if (maze[temp_row_idx][temp_col_idx - 1] == ' ')
+    {
+        temp_count++;
+    }
+    if (maze[temp_row_idx + 1][temp_col_idx - 1] == ' ')
+    {
+        temp_count++;
+    }
+    if (maze[temp_row_idx + 2][temp_col_idx - 1] == ' ')
+    {
+        temp_count++;
+    }
+    if (temp_count == 3)
+    {
+        return true;
+    }
+    return false;
+}
 void minerMoveLeft()
 {
-   string status = "Relax";//Variable to Store Status as is there any Wall Present at left Side
+    string status = "Relax"; // Variable to Store Status as is there any Wall Present at left Side
     for (int row_idx = 0; row_idx < maze_row; row_idx++)
     {
         for (int col_idx = 0; col_idx < maze_col; col_idx++)
         {
-            if (maze[row_idx][col_idx] == '0')
+
+            if (maze[row_idx][col_idx] == '/')
             {
-                if (maze[row_idx][col_idx - 2] == ' ')
-                {
-                    maze[row_idx][col_idx] = ' ';
-                    gotoxy(col_idx, row_idx);
-                    cout << maze[row_idx][col_idx];
-                    maze[row_idx][col_idx - 1] = '0';
-                    gotoxy(col_idx - 1, row_idx);
-                    cout << maze[row_idx][col_idx - 1];
-                }
-                else{
-                    status = "Alert";// changing status that there is wall present at left side
-                }
-            }
-            else if (maze[row_idx][col_idx] == '/')
-            {
-                if (status == "Relax")
+                if (maze[row_idx][col_idx - 1] == ' ')
                 {
                     maze[row_idx][col_idx] = ' ';
                     gotoxy(col_idx, row_idx);
@@ -93,6 +131,10 @@ void minerMoveLeft()
                     maze[row_idx][col_idx - 1] = '/';
                     gotoxy(col_idx - 1, row_idx);
                     cout << maze[row_idx][col_idx - 1];
+                }
+                else
+                {
+                    status = "Alert";
                 }
             }
             else if (maze[row_idx][col_idx] == '\\')
@@ -109,4 +151,39 @@ void minerMoveLeft()
             }
         }
     }
+}
+void minerMoveRight()
+{
+    for (int row_idx = 0; row_idx < maze_row; row_idx++)
+    {
+        for (int col_idx = maze_col - 1; col_idx >= 0; col_idx--)
+        {
+            if (maze[row_idx][col_idx] == '\\' && isRightMovePossible(row_idx, col_idx))
+            {
+                MoveRightStatus = "Relax";
+            }
+            if (MoveRightStatus == "Relax")
+            {
+                if (maze[row_idx][col_idx] == '/')
+                {
+                    maze[row_idx][col_idx] = ' ';
+                    gotoxy(col_idx, row_idx);
+                    cout << maze[row_idx][col_idx];
+                    maze[row_idx][col_idx + 1] = '/';
+                    gotoxy(col_idx + 1, row_idx);
+                    cout << maze[row_idx][col_idx + 1];
+                }
+                else if (maze[row_idx][col_idx] == '\\')
+                {
+                    maze[row_idx][col_idx] = ' ';
+                    gotoxy(col_idx, row_idx);
+                    cout << maze[row_idx][col_idx];
+                    maze[row_idx][col_idx + 1] = '\\';
+                    gotoxy(col_idx + 1, row_idx);
+                    cout << maze[row_idx][col_idx + 1];
+                }
+            }
+        }
+    }
+    MoveRightStatus = "Alert";
 }
